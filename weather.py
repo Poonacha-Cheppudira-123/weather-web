3a779d3dce9068b0505b4ab0c511a7b9ab7aa9bc
poonacha <poonacha@pop-os.localdomain>
2023-08-23 22:56:24 -0500

    FInished basic functionality and style of WeatherWeb

---
+++ weather.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+
+def get_current_weather(city: str = "San Antonio") -> any:
+    url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    weather_data = requests.get(url).json()
+
+    return weather_data
